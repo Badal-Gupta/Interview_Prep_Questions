32c586ad72b9073c5fe80e15d9381a7e0080614c
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-01-27 20:26:53 +0530

    Time: 2116 ms (18.96%), Space: 12.5 MB (99.19%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        l=len(nums)
+        for i in range (0,l):
+            for j in range (i+1,l):
+                if(nums[i]+nums[j]==target):
+                    return [i,j]
+Solution()            
+        
