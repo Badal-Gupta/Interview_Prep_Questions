a66db8c652eeb9bee69e1ffb53ebd14b71797274
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-02-10 23:23:54 +0530

    Time: 18 ms (10.68%), Space: 17.4 MB (66.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,45 +1,56 @@
 class Solution {
 public:
-    bool find(TreeNode* root, TreeNode* target,vector<TreeNode*>&a){
-        if(root==NULL){
-            return false;
-        }
-        a.push_back(root);
-        if(root==target){
-            return true;
-        }
-        
-        if (find(root->left, target, a) || find(root->right, target, a))
-            return true;
-        a.pop_back();
-        return false;
-    }
+    // bool find(TreeNode* root, TreeNode* target,vector<TreeNode*>&a){
+    //     if(root==NULL){
+    //         return false;
+    //     }
+    //     a.push_back(root);
+    //     if(root==target){
+    //         return true;
+    //     }
+    //     if (find(root->left, target, a) || find(root->right, target, a))
+    //         return true;
+
+    //     a.pop_back();
+    //     return false;
+    // }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>a;
-        vector<TreeNode*>b;
-        find(root,p,a);
-        find(root,q,b);
-        int x = a.size();
-        int y = b.size();
-        int different=-1;
-        if(x>=y){
-            for(int i=0;i<y;i++){
-                if(a[i]==b[i]){
-                    different=i;
-                }
-            }
-            if(different!=-1)return b[different];
-            else return root;
-        }else{
-            for(int i=0;i<x;i++){
-                if(a[i]==b[i]){
-                    different=i;
-                }
-            }
-            if(different!=-1)return a[different];
-            else return root;
+        // vector<TreeNode*>a;
+        // vector<TreeNode*>b;
+        // find(root,p,a);
+        // find(root,q,b);
+        // int x = a.size();
+        // int y = b.size();
+        // int different=-1;
+        // if(x>=y){
+        //     for(int i=0;i<y;i++){
+        //         if(a[i]==b[i]){
+        //             different=i;
+        //         }
+        //     }
+        //     if(different!=-1)return b[different];
+        //     else return root;
+        // }else{
+        //     for(int i=0;i<x;i++){
+        //         if(a[i]==b[i]){
+        //             different=i;
+        //         }
+        //     }
+        //     if(different!=-1)return a[different];
+        //     else return root;
+        // }
+        if(root==NULL||root==p||root==q){
+            return root;
         }
-        
-    
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+
+        if(left==NULL){
+            return right;
+        }else if(right==NULL){
+            return left;
+        }else{
+            return root;
+        }
     }
 };
