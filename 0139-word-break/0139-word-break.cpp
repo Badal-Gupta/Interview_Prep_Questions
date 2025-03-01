072706c4a74aa8a5560ed0c59708fa6ee336d3ac
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-03-01 14:49:28 +0530

    Time: 15 ms (34.26%), Space: 17.6 MB (27.21%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,48 @@
+// class Solution {
+// public:
+//     bool wordBreak(string s, vector<string>& wordDict) {
+        
+//         int n = s.length();
+//         int m = wordDict.size();
+//         vector<int>vis(m,0);
+//         // while(s.length()>0 && m>0){
+//             for(int i = 0;i<wordDict.size();i++){
+//                 int a = s.find(wordDict[i]);
+//                 if(a>=0 && a<n){
+//                     int b=wordDict[i].length();
+//                     s.erase(s.begin()+a , s.begin()+a+b);
+//                     vis[i]=1;
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//         // }
+//         for(int i=0; i<wordDict.size(); i++){
+//             if(vis[i]==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        
-        int n = s.length();
-        int m = wordDict.size();
-        vector<int>vis(m,0);
-        // while(s.length()>0 && m>0){
-            for(int i = 0;i<wordDict.size();i++){
-                int a = s.find(wordDict[i]);
-                if(a>=0 && a<n){
-                    int b=wordDict[i].length();
-                    s.erase(s.begin()+a , s.begin()+a+b);
-                    vis[i]=1;
-                }else{
-                    return false;
+        unordered_set<string>st;
+        for(auto i : wordDict){
+            st.insert(i);
+        }
+        vector<int>dp(s.length()+1,0);
+        dp[0]=1;
+        for(int i =0;i<=s.length();i++){
+            for(int j = 0;j<i;j++){
+                string a = s.substr(j,i-j);
+                if(dp[j]&&st.find(a)!=st.end()){
+                    dp[i]=1;
+                    break;
                 }
             }
-        // }
-        for(int i=0; i<wordDict.size(); i++){
-            if(vis[i]==0){
-                return false;
-            }
         }
-        return true;
+        return dp[s.size()];
     }
 };
