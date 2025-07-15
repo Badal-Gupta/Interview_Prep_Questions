23d7a4c9e1fce89fb87f052865d34f51e74e5aed
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-07-15 09:38:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.14%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     bool isValid(string word) {
         int n = word.length();
-        if(n<3){
+        if (n < 3) {
             return false;
         }
         for (auto& x : word) {
             x = tolower(x);
         }
-        int vowel =0;
-        int consonant = 0;
-        for(char a:word){
-            if(!isalnum(a)){
+        bool vowel = false;
+        bool consonant = false;
+        for (char a : word) {
+            if (!isalnum(a)) {
                 return false;
-            }else{
-                if(isalpha(a)){
-                if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
-                    vowel++;
-                }else{
-                    consonant++;
-                }
+            } else {
+                if (isalpha(a)) {
+                    if (a == 'a' || a == 'e' || a == 'i' || a == 'o' ||
+                        a == 'u') {
+                        vowel=true;
+                    } else {
+                        consonant=true;
+                    }
                 }
             }
         }
-        if(vowel<1 || consonant <1){
-            return false;
+        if (vowel && consonant) {
+            return true;
         }
-        return true;
+        return false;
     }
 };
