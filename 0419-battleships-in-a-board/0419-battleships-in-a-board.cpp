45385288fb0a9158bf2db244b9f62b95fe4ab7c8
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-07-17 10:03:45 +0530

    Time: 0 ms (100%), Space: 14.6 MB (8.82%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>&arr , int row , int col , int n , int m , vector<vector<bool>>&vis){
+        if(row<0 || col<0 || row>=n || col >=m || vis[row][col] || arr[row][col]=='.'){
+            return ;
+        }
+        vis[row][col]=true;
+        vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
+        for(int i = 0 ;i <4;i++){
+            int nr = row + dir[i].first;
+            int nc = col + dir[i].second;
+            dfs(arr,nr,nc,n,m,vis);
+        }
+    }
+    int countBattleships(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(board[i][j]=='X'&&!vis[i][j]){
+                    dfs(board,i,j,n,m,vis);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
