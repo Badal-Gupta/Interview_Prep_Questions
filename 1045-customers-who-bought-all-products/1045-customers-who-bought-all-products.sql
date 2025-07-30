a0748a076047aa03c4cfac19d43eac9a9fd9727a
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-07-31 00:09:51 +0530

    Time: 910 ms (6.43%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,11 @@
+-- Select customer_id from Customer 
+-- group by customer_id
+-- having count(distinct product_key) = (select count(*) from Product); 
+SELECT customer_id
+FROM (
+    SELECT customer_id, product_key
+    FROM Customer
+    GROUP BY customer_id, product_key
+) a
+GROUP BY customer_id
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Product);
