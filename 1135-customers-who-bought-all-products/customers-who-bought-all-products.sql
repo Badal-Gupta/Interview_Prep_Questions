251a687b4f87a144eb4a8fc6601d995ec2d7039c
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-07-31 00:05:51 +0530

    Time: 558 ms (58.41%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,3 +1,11 @@
-Select customer_id from Customer 
-group by customer_id
-having count(distinct product_key) = (select count(*) from Product); 
+-- Select customer_id from Customer 
+-- group by customer_id
+-- having count(distinct product_key) = (select count(*) from Product); 
+SELECT customer_id
+FROM (
+    SELECT customer_id, product_key
+    FROM Customer
+    GROUP BY customer_id, product_key
+) AS grouped_pairs
+GROUP BY customer_id
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Product);
