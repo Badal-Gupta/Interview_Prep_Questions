12cc5a905249e0b1833dccf22a1cc3522cf86fa0
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-08-03 15:19:00 +0530

    Time: 394 ms (99.82%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,3 +1,3 @@
 select customer_number from orders 
- group by customer_number
- order by count(customer_number) desc limit 1;
+   group by customer_number
+   order by count(customer_number) desc limit 1;
