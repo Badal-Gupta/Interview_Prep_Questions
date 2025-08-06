0e21753a71c6a02cea37e1b377dce4c66f0a45f5
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-08-06 23:15:44 +0530

    Time: 227 ms (72.41%), Space: 10.7 MB (66.71%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    bool search(int index,int i,int j,int n,int m,vector<vector<char>>&board, string word){
-        if(index == word.length()){
+    bool dfs(int row, int col, int n, int m, int ind, string& word, int size,
+             vector<vector<char>>& board, vector<vector<bool>>& vis) {
+
+        if (ind == size) {
             return true;
         }
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='$'){
+
+        if (row < 0 || col < 0 || row >= n || col >= m || vis[row][col] || board[row][col] != word[ind]) {
             return false;
         }
-        if(word[index]!=board[i][j]){
-            return false;
-        }
-        char temp  = board[i][j];
-        board[i][j]='$';
-        if(search(index+1,i+1,j,n,m,board,word)||search(index+1,i-1,j,n,m,board,word)||search(index+1,i,j+1,n,m,board,word)||search(index+1,i,j-1,n,m,board,word)){
-            return true;
-        }
-        board[i][j]=temp;
-        return false;
+
+        vis[row][col] = true;
+
+        bool found = dfs(row + 1, col, n, m, ind + 1, word, size, board, vis) ||
+                     dfs(row - 1, col, n, m, ind + 1, word, size, board, vis) ||
+                     dfs(row, col + 1, n, m, ind + 1, word, size, board, vis) ||
+                     dfs(row, col - 1, n, m, ind + 1, word, size, board, vis);
+
+        vis[row][col] = false; // backtrack
+
+        return found;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
+        int size = word.size();
 
-        for(int i =0;i<n;i++){
-            for(int j = 0 ;j<m;j++){
-                if(word[0]==board[i][j]&& search(0, i ,j, n,m , board, word)){
-                    return true;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(i, j, n, m, 0, word, size, board, vis)) {
+                        return true;
+                    }
                 }
             }
         }
