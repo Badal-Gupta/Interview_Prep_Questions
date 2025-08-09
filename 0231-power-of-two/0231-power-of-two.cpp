c8b0d4d9c68790a84e30ff23576d756b935847d8
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-08-10 01:58:02 +0530

    Time: 0 ms (100%), Space: 8 MB (17.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1){
-            return true;
-        }else if(n%2==1){
-            return false;
-        }
-        while(n>=2){
-            if(n==2){
-                return true;
-            }
-            n=n>>1;
-        }
-        return false;
+        return n>0 && not (n &(n-1));
     }
 };
