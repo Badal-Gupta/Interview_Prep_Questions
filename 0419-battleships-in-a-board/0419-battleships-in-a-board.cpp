9505e37146df0b29daa619375be703354c4e1b04
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-08-17 23:28:35 +0530

    Time: 0 ms (100%), Space: 12.1 MB (44.26%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -16,7 +16,6 @@ public:
         int n = board.size();
         int m = board[0].size();
 
-        // vector<vector<bool>>vis(n,vector<bool>(m,false));
         int count =0;
         for(int i=0;i<n;i++){
             for(int j = 0;j<m;j++){
