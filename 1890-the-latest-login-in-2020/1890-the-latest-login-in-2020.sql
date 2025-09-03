d23fafa2c16baaea81536d8885b19d1062d49cd0
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-09-03 21:09:04 +0530

    Time: 798 ms (15.97%), Space: 0B (100%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+Select (user_id) as user_id, max(time_stamp) as last_stamp from Logins
+where  ((time_stamp > "2020-01-01 00:00:00") and (time_stamp <"2021-01-01 00:00:00"))
+group by user_id;
