2b8d9331a395092e35ac3ff40dfcbaa48c0234dd
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-09-09 23:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (68.56%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    const int mod=1e9+7;
-
-    int peopleAwareOfSecret(int n, int delay, int forget) {
-        
-        vector<int> dp(n+1);
+    const int mod = 1e9+7; 
+    int peopleAwareOfSecret(int n, int d, int f) {
+        vector<int>dp(n+1);
         dp[1]=1;
-        int total=0;
+        int count = 0;
+        for(int i = 2; i<=n ;i++){
+            if(i-d>=1){
+                count = (count + dp[i-d])% mod;
+            }
+            if(i-f>=1){
+                count = (count - dp[i-f] + mod)% mod;
+            }
 
-        for(int i=2;i<=n;i++){
-            if(i-delay>=1)total=(total+dp[i-delay])%mod;
-            if(i-forget>=1)total=(total-dp[i-forget]+mod)%mod;
-
-            dp[i]=total;
+            dp[i] = count;
         }
 
-        int ans=0;
-        for(int i=n-forget+1;i<=n;i++){
-            ans=(ans+dp[i])%mod;
+        int people = 0;
+        for(int i = n-f+1 ; i<=n;i++){
+            people = (people + dp[i]) % mod;
         }
-        return ans;
+
+        return people;
+
+
     }
 };
