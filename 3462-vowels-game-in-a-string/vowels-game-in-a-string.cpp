6c639db3f8b0a5d48191432fa0a58a7f45328d19
Badal Gupta <120306731+Badal-Gupta@users.noreply.github.com>
2025-09-13 00:18:25 +0530

    Time: 46 ms (5.04%) | Memory: 19.9 MB (5.41%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int isVowel(char ch){
+        string str = "aeiouAEIOU";
+        return (str.find(ch) != string::npos);
+    }
+    bool doesAliceWin(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(isVowel(s[i])){
+                count++;
+            }
+        }
+        if(count>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
